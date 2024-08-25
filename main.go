eef4b35f07411caa08343d42075472c48fc9a244
Romasav <roma.savitski@gmail.com>
2024-08-25 16:41:11 +0100

    Add Fileserver

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 
 func main() {
 	serverMux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("."))
+
+	serverMux.Handle("/", fileServer)
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serverMux,
