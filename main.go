0f785fd96be03419d423657831de03007b372a7b
Romasav <roma.savitski@gmail.com>
2024-08-28 16:26:56 +0100

    Added Readiness handler

--- main.go
+++ main.go
@@ -4,16 +4,25 @@ import (
 	"net/http"
 )
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	serverMux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("."))
+	fileServer := http.FileServer(http.Dir(filepathRoot))
 
-	serverMux.Handle("/", fileServer)
-	serverMux.Handle("/assets/logo.png", fileServer)
+	serverMux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	serverMux.HandleFunc("/healthz", healthzHandler)
 
 	server := http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: serverMux,
 	}
 	server.ListenAndServe()
